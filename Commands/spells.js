17e090f8e933229111a0fbbf469c9dfb18deb5e9
Ubuntu <Glorysand@Glorysandbox.2bjqojdohgqurntnvgk0b2lpkc.bx.internal.cloudapp.net>
2019-03-29 18:05:49 +0000

    Added some files

---
+++ Commands/spells.js
@@ -0,0 +1,41 @@
+var functions = require("../Utils/functions.js")
+module.exports = function (message) 
+{
+    let id = message.author.id;
+
+    let itemName = ""
+    let itemNum = ""
+    itemName += "ID 1000 - Healing Spells\n"
+    itemNum += userData[id].spells[0] + "\n"
+    itemName += "ID 1001 - Damage Spells\n"
+    itemNum += userData[id].spells[1] + "\n"
+    itemName += "ID 1002 - Fortune Spells\n"
+    itemNum += userData[id].spells[2] + "\n"
+    itemName += "ID 1003 - Karma Spells\n"
+    itemNum += userData[id].spells[3] + "\n"
+    itemName += "ID 1004 - Meme Spells\n"
+    itemNum += userData[id].spells[4] + "\n"
+    
+
+    functions.sendMessage(message.channel, {
+      "embed": {
+        "title": "Spells.. idk change name later",
+        "description": "Here is your spell inventory\nUse `!cast [ID_of_Item]` to cast you spell... yeah \n",
+        "color": 13498074,
+
+        "fields": [
+          {
+            "name": "Spell",
+            "value": itemName,
+            "inline": true
+          },
+          {
+            "name": "Quantity",
+            "value": itemNum,
+            "inline": true
+          }
+        ]
+      }
+    })
+
+}
