0b223b4fc09814e444e961d4c7ed27b1e0aff102
bw55555 <iamsomeoneonearth@gmail.com>
2019-04-05 12:14:43 -0400

    Fixed return message

--- Commands/inventory.js
+++ Commands/inventory.js
@@ -7,8 +7,8 @@ module.exports = function (message) {
     pages = []
     page = ""
     displayItems = functions.itemFilter(message)
-    if (displayItems == false) { return }
     if (displayItems.length == 0) { return functions.sendMessage(message.channel, "There's nothing in your inventory that matches the selected filters... ") }
+    if (displayItems == false) { return }
     for (var i = 0; i < displayItems.length;i++) {
         let itemID = displayItems[i].toString();
         page = functions.generateWeaponTemplate(itemID, i+1, displayItems.length)
