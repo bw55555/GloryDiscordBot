893861403c748fc724dcb1a67e025314c16c33c1
bw55555 <iamsomeoneonearth@gmail.com>
2019-04-05 11:52:50 -0400

    Added unique filter, automatically doesn't display uniques.

--- Commands/inventory.js
+++ Commands/inventory.js
@@ -7,6 +7,7 @@ module.exports=function(message) {
   let minrarity = 0
   let maxrarity = 9
   let fav = "None"
+    let unique = false
   if (words.indexOf("-min") != -1) {
       minrarity = parseInt(words[words.indexOf("-min") + 1])
       if (isNaN(minrarity)) { return functions.replyMessage(message,"Please enter an integer for the minimum rarity.")}
@@ -15,13 +16,14 @@ module.exports=function(message) {
       minrarity = parseInt(words[words.indexOf("-max") + 1])
       if (isNaN(minrarity)) { return functions.replyMessage(message, "Please enter an integer for the maximum rarity.") }
   }
-  if (maxrarity < 0 || maxrarity > 9 || minrarity < 0 || minrarity > 9)
-    {return functions.replyMessage(message, "No items exist at that level! Choose different bounds")} 
   if (words.indexOf("-fav") != -1) {
       fav = true
   }
   if (words.indexOf("-nofav") != -1) {
       fav = false
+  }
+  if (words.indexOf("-unique") != -1) {
+      unique = true
   }
   let pages = []
   //console.log(userData[id].weapon)
@@ -36,6 +38,7 @@ module.exports=function(message) {
       //console.log(item)
       //console.log(itemData[item])
       let itemID = item.toString();
+      if (!unique && itemData[item].rarity == "Unique") { continue}
       if (itemData[itemID] == undefined || item == userData[id].weapon || userData[id].inventory[item] != item || itemData[itemID].rarity < minrarity || itemData[itemID].rarity > maxrarity) continue
       if (fav == true && itemData[itemID].favorite == false) { continue }
       if (fav == false && itemData[itemID].favorite == true) { continue }
