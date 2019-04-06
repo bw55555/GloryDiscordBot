ba5c51016cc7620808533b7abaeb9bfa5d2c3f64
bw55555 <iamsomeoneonearth@gmail.com>
2019-04-05 23:16:42 -0400

    New tutorial

--- Commands/tutorial.js
+++ Commands/tutorial.js
@@ -3,5 +3,5 @@ module.exports=function(message) {
   let id = message.author.id;
   let ts = message.createdTimestamp;
   let words = message.content.trim().split(/\s+/)
-  functions.sendMessage(message.channel, "The tutorial can be found at https://1drv.ms/w/s!AoBnNmSCqpWMaiCEqZmE6ecm2PQ")
+  functions.sendMessage(message.channel, "The tutorial can be found at https://docs.google.com/document/d/1OtNO65uXrREcPiBi8Bn59yYDci4w2hwcSrRrGID5HAY/edit")
 }
