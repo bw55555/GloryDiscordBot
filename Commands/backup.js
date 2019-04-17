b2f860412bfe3cb50ea066b921a664f0fd550ec5
bw55555 <iamsomeoneonearth@gmail.com>
2019-04-17 10:29:56 -0400

    backup creates a new folder now

--- Commands/backup.js
+++ Commands/backup.js
@@ -5,14 +5,14 @@ module.exports=function(message) {
   let words = message.content.trim().split(/\s+/)
   if (admins.indexOf(id)==-1){return}
   functions.replyMessage(message, "Backing up data...")
-  fs.writeFileSync('AutoBackup/' + ts + '_userData.json', JSON.stringify(userData, null, 4))//.then(sendMessage(message.channel,"userData backed up!"))
-  fs.writeFileSync('AutoBackup/' + ts + '_itemData.json', JSON.stringify(itemData, null, 4))//.then(sendMessage(message.channel,"itemData backed up!"))
-  fs.writeFileSync('AutoBackup/' + ts + '_mobData.json', JSON.stringify(mobData, null, 4))//.then(sendMessage(message.channel,"mobData backed up!"))
-  fs.writeFileSync('AutoBackup/' + ts + '_guildData.json', JSON.stringify(guildData, null, 4))//.then(sendMessage(message.channel,"guildData backed up!"))
-  fs.writeFileSync('AutoBackup/' + ts + '_serverData.json', JSON.stringify(serverData, null, 4))//.then(sendMessage(message.channel,"guildData backed up!"))
-  fs.writeFileSync('AutoBackup/' + ts + '_devData.json', JSON.stringify(devData, null, 4))//.then(sendMessage(message.channel,"guildData backed up!"))
-  fs.writeFileSync('AutoBackup/' + ts + '_questData.json', JSON.stringify(questData, null, 4))//.then(sendMessage(message.channel,"guildData backed up!"))
-
+  fs.mkdirSync(ts)
+  fs.writeFileSync('AutoBackup/' + ts + '/userData.json', JSON.stringify(userData, null, 4))//.then(sendMessage(message.channel,"userData backed up!"))
+  fs.writeFileSync('AutoBackup/' + ts + '/itemData.json', JSON.stringify(itemData, null, 4))//.then(sendMessage(message.channel,"itemData backed up!"))
+  fs.writeFileSync('AutoBackup/' + ts + '/mobData.json', JSON.stringify(mobData, null, 4))//.then(sendMessage(message.channel,"mobData backed up!"))
+  fs.writeFileSync('AutoBackup/' + ts + '/guildData.json', JSON.stringify(guildData, null, 4))//.then(sendMessage(message.channel,"guildData backed up!"))
+  fs.writeFileSync('AutoBackup/' + ts + '/serverData.json', JSON.stringify(serverData, null, 4))//.then(sendMessage(message.channel,"guildData backed up!"))
+  fs.writeFileSync('AutoBackup/' + ts + '/devData.json', JSON.stringify(devData, null, 4))//.then(sendMessage(message.channel,"guildData backed up!"))
+  fs.writeFileSync('AutoBackup/' + ts + '/questData.json', JSON.stringify(questData, null, 4))//.then(sendMessage(message.channel,"guildData backed up!"))
   functions.replyMessage(message, "Data backed up!")
   functions.logCommand(message)
 }
