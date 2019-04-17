8e6f5ae76ce960df7f35e5d1139edbf2326f19fb
GloryMachineUser <iamsomeoneonearth@gmail.com>
2019-04-17 19:38:47 +0000

    Backup creates a folder

--- Commands/backup.js
+++ Commands/backup.js
@@ -5,7 +5,7 @@ module.exports=function(message) {
   let words = message.content.trim().split(/\s+/)
   if (admins.indexOf(id)==-1){return}
   functions.replyMessage(message, "Backing up data...")
-  fs.mkdirSync(ts)
+  fs.mkdirSync('AutoBackup/'+ts)
   fs.writeFileSync('AutoBackup/' + ts + '/userData.json', JSON.stringify(userData, null, 4))//.then(sendMessage(message.channel,"userData backed up!"))
   fs.writeFileSync('AutoBackup/' + ts + '/itemData.json', JSON.stringify(itemData, null, 4))//.then(sendMessage(message.channel,"itemData backed up!"))
   fs.writeFileSync('AutoBackup/' + ts + '/mobData.json', JSON.stringify(mobData, null, 4))//.then(sendMessage(message.channel,"mobData backed up!"))
