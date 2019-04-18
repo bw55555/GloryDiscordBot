0b1badb8fd92da4a1f3db78d9d88163ac12badd0
Nix <48928368+Nixolium@users.noreply.github.com>
2019-04-18 08:00:34 -0500

    Richest now uses paginator

--- Commands/richest.js
+++ Commands/richest.js
@@ -1,98 +1,83 @@
-var functions=require("../Utils/functions.js")
-module.exports=function(message) {
-  let id = message.author.id;
-  let ts = message.createdTimestamp;
-  let words = message.content.trim().split(/\s+/)
-  var globalMoney = 0;
-  var globalUsers = 0;
-  var globalRichest = '';
-  var globalRichestId = '';
-  var globalRichestMoney = 0;
-  var globalRichest2 = '';
-  var globalRichestId2 = '';
-  var globalRichestMoney2 = 0;
-  var globalRichest3 = '';
-  var globalRichestId3 = '';
-  var globalRichestMoney3 = 0;
-  var globalRichest4 = '';
-  var globalRichestId4 = '';
-  var globalRichestMoney4 = 0;
-  for (var i in userData) {//loops through user data to check. //w/o if statement, it checks everyone
-    globalMoney += userData[i].money; //adds money to total money
-    globalUsers += 1; //checks guilds
-    if (userData[i].money > globalRichestMoney) { //checks if user money is greater than richest
-      globalRichestMoney4 = globalRichestMoney3;
-      globalRichestId4 = globalRichestId3;
-      globalRichest4 = globalRichest3;
-
-      globalRichestMoney3 = globalRichestMoney2;
-      globalRichestId3 = globalRichestId2;
-      globalRichest3 = globalRichest2;
-
-      globalRichestMoney2 = globalRichestMoney;
-      globalRichestId2 = globalRichestId;
-      globalRichest2 = globalRichest;
-
-      globalRichestMoney = userData[i].money;
-      globalRichestId = i;
-      globalRichest = userData[i].username;
+var functions = require("../Utils/functions.js")
+module.exports = function (message) {
+    let id = message.author.id;
+    //let ts = message.createdTimestamp;
+    //let words = message.content.trim().split(/\s+/)
+    let globalUsers = 0
+    let globalWealth = 0;
+    let xparrtosort = []
+    for (var i in guildData){
+      globalWealth += guildData[i].bank;
     }
-    else if (userData[i].money > globalRichestMoney2) {
-      globalRichestMoney4 = globalRichestMoney3;
-      globalRichestId4 = globalRichestId3;
-      globalRichest4 = globalRichest3;
-
-      globalRichestMoney3 = globalRichestMoney2;
-      globalRichestId3 = globalRichestId2;
-      globalRichest3 = globalRichest2;
-
-      globalRichestMoney2 = userData[i].money;
-      globalRichestId2 = i;
-      globalRichest2 = userData[i].username;
+    for (var userid in userData) {//loops through user data to check. //w/o if statement, it checks everyone
+        if (userData[userid].money != undefined) {
+            xparrtosort.push(userData[userid].money + " " + userid) // push level + 100*ascensions for sorting purposes. Add the " " and the id for identification.
+            globalUsers += 1;
+            globalWealth += userData[userid].money;
+        }
     }
-    else if (userData[i].money > globalRichestMoney3) {
-      globalRichestMoney4 = globalRichestMoney3;
-      globalRichestId4 = globalRichestId3;
-      globalRichest4 = globalRichest3;
-
-      globalRichestMoney3 = userData[i].money;
-      globalRichestId3 = i;
-      globalRichest3 = userData[i].username;
+    xparrtosort.sort(function (a, b) { return parseInt(b.split(" ")[0]) - parseInt(a.split(" ")[0]) }) //what sorts the array. Search up array.sort() on w3schools.
+    let numPerPage = 10
+    let page = {
+        "embed": { //displays guild stats
+            "title": "Global Levels",
+            "color": 0xF1C40F,
+            "fields": [{
+                "name": "Highest Level Accounts",
+                "value": ""
+            }],
+            "footer": {
+                "text": ""
+            },
+        }
     }
-    else if (userData[i].money > globalRichestMoney4) {
-      globalRichestMoney4 = userData[i].money;
-      globalRichestId4 = i;
-      globalRichest4 = userData[i].username;
+    let pages = []
+    for (var i = 0; i < globalUsers; i++) {
+        let user = xparrtosort[i].split(" ")
+        let text = parseInt(user[0])
+        //let leveltext = parseInt(user[0]) - 100 * asctext
+        let username = userData[user[1]].username
+        page.embed.fields[0].value += "**" + (i + 1) + ". " + username + "** (ID: " + user[1] + ") with **$" + text + "**"
+        if (i % numPerPage == numPerPage - 1) { // separate pages
+            page.embed.footer.text = (pages.length * numPerPage + 1) + "-" + (i + 1) + " out of " + globalUsers //add footer to display where you are
+            pages.push(page)
+            page = {
+                "embed": { //displays guild stats
+                    "title": "Global Wealth",
+                    "color": 0xF1C40F,
+                    "fields": [{
+                        name: "Money in Economy",
+                        value: globalWealth,
+                        inline: true
+                     },{
+                        "name": "Richest Accounts",
+                        "value": ""
+                    }],
+                    "footer": {
+                        "text": ""
+                    },
+                }
+            }
+        } else {
+            page.embed.fields[0].value += "\n"
+        }
     }
-  }
-
-  for (var i in guildData){
-    globalMoney += guildData[i].bank;
-  }
-
-  //closes if statements
-
-  functions.sendMessage(message.channel, {
-    embed: { //displays guild stats
-      title: "GlobalStats",
-      color: 0xF1C40F,
-      fields: [{ //displays variables
-        name: "Total Accounts",
-        value: globalUsers,
-        inline: true
-      },
-      {
-        name: "Money in Economy",
-        value: `$` + globalMoney,
-        inline: true
-      }, {
-        name: "Richest Account",
-        value: `**${globalRichest}** with id ${globalRichestId} and **$${globalRichestMoney}**\n` +
-          `**${globalRichest2}** with id ${globalRichestId2} and **$${globalRichestMoney2}**\n` +
-          `**${globalRichest3}** with id ${globalRichestId3} and **$${globalRichestMoney3}**\n` +
-          `**${globalRichest4}** with id ${globalRichestId4} and **$${globalRichestMoney4}**\n` //displays richest person.
-      }
-      ]
+    if (page.embed.fields[0] != "") {
+        page.embed.footer.text = (pages.length * numPerPage + 1) + "-" + (i + 1) + " out of " + globalUsers
+        pages.push(page)
+        page = {
+            "embed": { //displays guild stats
+                "title": "Global Wealth",
+                "color": 0xF1C40F,
+                "fields": [{
+                    "name": "Richest Accounts",
+                    "value": ""
+                }],
+                "footer": {
+                    "text": ""
+                },
+            }
+        }
     }
-  });
+    new functions.Paginator(message.channel, message.author, pages)
 }
