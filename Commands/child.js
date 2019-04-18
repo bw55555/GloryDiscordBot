7132e14389f7b49a9cb5b6dd7208411de5d7a435
Nix <48928368+Nixolium@users.noreply.github.com>
2019-04-18 08:10:04 -0500

    jokes in !child

--- Commands/child.js
+++ Commands/child.js
@@ -3,5 +3,5 @@ module.exports=function(message) {
   let id = message.author.id;
   let ts = message.createdTimestamp;
   let words = message.content.trim().split(/\s+/)
-  functions.sendMessage(message.channel, "This feature is under development... Check back later or join the support server for updates on progress.");
+  functions.sendMessage(message.channel, "Da heck u do dis for?");
 }
