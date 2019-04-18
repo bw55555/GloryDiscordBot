d2fcaf3c8c68408ddbd919941b7d724a93ad5fc8
Nix <48928368+Nixolium@users.noreply.github.com>
2019-04-18 08:08:16 -0500

    Update daily.js

--- Commands/daily.js
+++ Commands/daily.js
@@ -7,7 +7,7 @@ module.exports=function(message) {
   if (userData[id].lastDaily != moment().format('L')) {//Checks if last daily object is L
     userData[id].lastDaily = moment().format('L') //Sets daily as L
     let boxNumber = Math.floor(userData[id].glory/10) + 1
-    let userData[id].bounty += Math.floor(0.5 * userData[id].glory * (userData[id].ascension * 100 + userData[id].level - 1));
+    let userData[id].bounty += Math.floor(0.1 * userData[id].glory * (userData[id].ascension * 100 + userData[id].level - 1));
     functions.consumGive(id, "box", boxNumber); //gives you one box
     functions.sendMessage(message.channel, "You received " + boxNumber + " mysterious box(es)... what could it contain?");
 	userData[id].glory += 0.25
