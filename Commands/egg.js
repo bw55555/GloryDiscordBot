f826544f1ee3334380c2bf9c1280bf0c859d306b
bw55555 <iamsomeoneonearth@gmail.com>
2019-04-19 11:47:06 -0400

    Fixed !egg to dm the user and not the message

--- Commands/egg.js
+++ Commands/egg.js
@@ -4,14 +4,14 @@ module.exports = function (message) {
     //let ts = message.createdTimestamp;
     let words = message.content.split(/\s+/)
     functions.deleteMessage(message);
-    if (words[1] == undefined) { return functions.dmUser(message, "All easter eggs are 7 characters long!") }
+    if (words[1] == undefined) { return functions.dmUser(id, "All easter eggs are 7 characters long!") }
     let attempt = words[1].toUpperCase()
     if (attempt.length != 7) {
-        return functions.dmUser(message, "All easter eggs are 7 characters long!")
+        return functions.dmUser(id, "All easter eggs are 7 characters long!")
     }
-    if (eggData[attempt] == undefined) { return functions.dmUser(message, "You tried this code... and nothing happened") }
+    if (eggData[attempt] == undefined) { return functions.dmUser(id, "You tried this code... and nothing happened") }
     let reward = eggData[attempt].reward
-    if (eggData[attempt].claimed == true) { return functions.dmUser(message, "This code has been claimed already!") }
+    if (eggData[attempt].claimed == true) { return functions.dmUser(id, "This code has been claimed already!") }
     if (reward == "item") {
         let rewardItemData = eggData[attempt].amount
         functions.generateItem(id, itemData.next, rewardItemData.attack, rewardItemData.defense, rewardItemData.rarity, rewardItemData.name, rewardItemData.modifiers)
@@ -19,5 +19,5 @@ module.exports = function (message) {
     if (userData[id][reward] != undefined) { userData[id][reward] += eggData[attempt].amount }
     if (userData[id].consum[reward] != undefined) { userData[id].consum[reward] += eggData[attempt].amount }
     if (attempt != "EXAMPLE") { eggData[attempt].claimed = true }
-    functions.dmUser(message, eggData[attempt].text)
+    functions.dmUser(id, eggData[attempt].text)
 }
