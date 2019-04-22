d98fb2f5a69e87bc23e22e2f71bd4878f428ef09
bw55555 <iamsomeoneonearth@gmail.com>
2019-04-22 16:03:44 -0400

    fixed a bug in adminset

--- Commands/admincheck.js
+++ Commands/admincheck.js
@@ -9,10 +9,27 @@ module.exports = function (message) {
 
 
     let attribute = words[2];
+    if (attribute == undefined) { return functions.replyMessage(message,"Please specify an attribute to set!")}
     if (userData[target][attribute] != undefined) {
+        if (typeof userData[target][attribute] == "object") {
+            if (words.length > 3) {
+                let secondattribute = words[3]
+                if (userData[target][attribute][secondattribute] == undefined) {
+                    functions.sendMessage(message.channel, "undefined");
+                    return
+                }
+                functions.sendMessage(message.channel, attribute + ":" + secondattribute + "\n" + userData[target][attribute][secondattribute]);
+                functions.logCommand(message)
+                return
+            }
+            functions.sendMessage(message.channel, attribute + "\n" + JSON.stringify(userData[target][attribute]));
+            functions.logCommand(message)
+            return
+        }
         functions.sendMessage(message.channel, attribute + "\n" + userData[target][attribute]);
     } else {
         functions.sendMessage(message.channel, "undefined");
+        functions.logCommand(message)
         return;
     }
     functions.logCommand(message)
