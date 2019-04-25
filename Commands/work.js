f1aa9c0986be27c6fd2cc54cd68504fa12d46fc4
GloryMachineUser <iamsomeoneonearth@gmail.com>
2019-04-25 15:53:56 +0000

    Fixed bugs

--- Commands/work.js
+++ Commands/work.js
@@ -21,9 +21,10 @@ module.exports = function (message) {
   }
   earnings*=functions.calcLuckyBuff(id)
   earnings = Math.floor(earnings)
+  userData[id].xp += earnings;
   userData[id].money += earnings;
   userData[id].cooldowns.work = ts + workcdseconds * 1000
-  text += message.author.username + ' worked for $' + earnings + '! '
+  text += message.author.username + ' worked for ' + earnings + ' money and xp!'
 
   if (userData[id].marry != "None") {
     let spouse = userData[id].marry;
