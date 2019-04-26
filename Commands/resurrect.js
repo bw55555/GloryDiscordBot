6068a62bd2db1cd926b64994a655b036d4f8e409
GloryMachineUser <iamsomeoneonearth@gmail.com>
2019-04-26 17:09:58 +0000

    misc bug fix

--- Commands/resurrect.js
+++ Commands/resurrect.js
@@ -8,7 +8,7 @@ module.exports = function (message) {
       functions.replyMessage(message, "You're not dead. Why do you need to rez?");
       return;
     }
-    if (userData[id].health-10 >= functions.calcExtraStat(id, "health") && userData[id].health >= 10) { //debuffs stats if they are above a certain level
+    if (userData[id].health-10 >= functions.calcExtraStat(id, "health") && userData[id].health > 10) { //debuffs stats if they are above a certain level
       userData[id].health -= 10;
     }
     if (userData[id].attack - 1 >= functions.calcExtraStat(id, "attack") && userData[id].attack >= 1) {
