57088dbbb4bd4889487b705fc4449faac4bee076
GloryMachineUser <iamsomeoneonearth@gmail.com>
2019-05-10 02:40:25 +0000

    Added eatk, improved eval

---
+++ Commands/eatk.js
@@ -0,0 +1,4 @@
+var alias=require("./eventattack.js")
+module.exports=function(message) {
+  return alias(message)
+}
--- Commands/eval.js
+++ Commands/eval.js
@@ -8,10 +8,15 @@ module.exports=function(message) {
   //console.log(message.channel.type)
   if (message.channel.type != "dm" && message.channel.type != "group") {prefix = serverData[message.guild.id].prefix}
   try {
-    const code = message.content.slice(prefix.length+5)
-    let evaled = eval(code);
-    if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-    message.channel.send(functions.clean(evaled), { code: "xl" });
+      let code = message.content.slice(prefix.length + 5)
+      let colonindex = code.lastIndexOf(";")
+      let ret = code.slice(colonindex + 1)
+      if (!ret.trim().startsWith("return ")) { ret = "return " + ret }
+      code = code.slice(0,colonindex+1)+ret
+      //let evaled = eval(code);
+      let evaled = eval('(message) => {' + code + '}')(message);
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+      message.channel.send(functions.clean(evaled), { code: "xl" });
   } catch (err) {
     message.channel.send(`\`ERROR\` \`\`\`xl\n`+functions.clean(err)+`\n\`\`\``);
   }
