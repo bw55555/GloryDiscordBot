87ea926a46e772345aaafdb4fc220d7af74e4300
GloryMachineUser <iamsomeoneonearth@gmail.com>
2019-05-29 02:43:35 +0000

    Added market and related functions

--- Commands/buy.js
+++ Commands/buy.js
@@ -1,4 +1,43 @@
-var alias=require("./purchase.js")
-module.exports=function(message) {
-  return alias(message)
+var functions = require("../Utils/functions.js")
+module.exports = function (message) {
+
+    let id = message.author.id;
+    if (userData[id].status == 0) { return; }
+    //let ts = message.createdTimestamp;
+    let words = message.content.split(/\s+/)
+    if (words.length != 2) {
+        functions.replyMessage(message, "`buy [weaponid]`")
+        return
+    }
+
+    if (isNaN(words[1])) {
+        functions.replyMessage(message, "Choose a real weapon id.")
+        return;
+    }
+    let weaponid = words[1]
+    if (itemData[weaponid] == undefined) {
+        if (userData[id].inventory[weaponid] == weaponid) {
+            delete userData[id].inventory[weaponid]
+        }
+        functions.replyMessage(message, "This weapon does not exist.");
+        return
+    } else if (userData[id].weapon == weaponid) {
+        functions.replyMessage(message, "You already have this weapon equipped. (Somehow?!?)")
+        return
+    } else if (itemData[weaponid].price == undefined) {
+        functions.replyMessage(message, "This item is not for sale.")
+        return
+    } else if (userData[id].money < itemData[weaponid].price) {
+        functions.replyMessage(message, "You don't have enough money to buy this item.")
+        return
+    }
+
+    userData[id].money -= parseInt(itemData[weaponid].price)
+    userData[itemData[weaponid].owner].money += parseInt(itemData[weaponid].price) //transfer of money
+    let previousOwner = itemData[weaponid].owner
+    itemData[weaponid].owner = id //weapon ownerid transfer
+    userData[id].inventory[weaponid] = weaponid //weapon added to inventory
+    functions.sendMessage(message.channel, "You bought " + weaponid + " for $" + itemData[weaponid].price)
+    functions.dmUser(previousOwner, "<@" + id + "> bought " + itemData[weaponid].name + " (" + weaponid + ") for $" + itemData[weaponid].price)
+    delete itemData[weaponid].price
 }
--- Commands/inv.js
+++ Commands/inv.js
@@ -1,4 +1,96 @@
-var alias=require("./inventory.js")
-module.exports=function(message) {
-  return alias(message)
+/*var functions = require("../Utils/functions.js")
+var display = require("../Utils/display.js")
+module.exports = function (message) {
+    let id = message.author.id;
+    let ts = message.createdTimestamp;
+    let words = message.content.split(/\s+/)
+    if (devs.indexOf(id) == -1) { return }
+    display.generateWeapon(id, "Test Spear", 16, -1, "This is a test sword.", -1)
+}*/
+
+var functions = require("../Utils/functions.js")
+module.exports = function (message) {
+    let id = message.author.id;
+    //let ts = message.createdTimestamp;
+    //let words = message.content.trim().split(/\s+/)
+    let weaponCount = 0
+    let wepsra = []
+
+    for (var weaponid in userData[id].inventory) {
+        if (weaponid != undefined) {
+            wepsra.push(weaponid)
+            weaponCount += 1;
+        }
+    }
+    wepsra.sort() //what sorts the array. Search up array.sort() on w3schools.
+    let numPerPage = 5
+    let pages = []
+    if (wepsra.length == 0) {
+        page = {
+            "embed": {
+                //"title": "Global Wealth",
+                "color": 0xffffff,
+                "fields": [
+                    {
+                        "name": "Empty Inventory",
+                        "value": "Why don't you go get some weapons?",
+                        "inline": true
+                    }
+                ],
+                "footer": {
+                    "text": "Page 1 of 1"
+                }
+            }
+        }
+    } else {
+        let infoText = ""
+        let totalText = ""
+        for (var i = 0; i <= weaponCount; i += 1) {
+            if (i % numPerPage != numPerPage - 1 && i != weaponCount) {
+                if (wepsra[i] != undefined) {
+                    infoText = displayWeaponText(wepsra[i])
+                    infoText += "\n"
+                    totalText += infoText
+                }
+            } else {
+                if (wepsra[i] != undefined) {
+                    infoText = displayWeaponText(wepsra[i])
+                    infoText += "\n"
+                    totalText += infoText
+                }
+                if (totalText != "") {
+                    page = {
+                        "embed": {
+                            //"title": "Global Wealth",
+                            "color": 0xffffff,
+                            "fields": [
+                                {
+                                    "name": userData[id].username + "'s Inventory",
+                                    "value": totalText,
+                                    "inline": true
+                                }
+                            ],
+                            "footer": {
+                                "text": "Page " + (pages.length + 1) + " of " + (1 + Math.floor(weaponCount / 5))
+                            },
+                        }
+                    }
+                    pages.push(page)
+                    totalText = ""
+                }
+            }
+        }
+    }
+    new functions.Paginator(message.channel, message.author, pages)
+}
+
+function displayWeaponText(id) {
+    let infoText = ""
+    infoText += "**" + itemData[id].name + "** (" + itemData[id].id + ")\n"
+    infoText += "Rarity: " + itemData[id].rarity + "\n"
+    infoText += "Atk +" + itemData[id].attack + " / Def +" + itemData[id].defense + "\n"
+    if (itemData[id].modifiers != undefined && itemData[id].modifiers.length > 0) {
+        infoText += "*Modifiers:* " + itemData[id].modifiers + "\n"
+    }
+    return infoText
 }
