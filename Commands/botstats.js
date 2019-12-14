0dfcc4d00ee1cf5fc5656255bb145f3bc755263d
Brian Wang <wanbr@utschools.ca>
2019-12-13 19:22:30 -0500

    added botstats

--- Commands/botstats.js
+++ Commands/botstats.js
@@ -1,26 +1,27 @@
 var functions=require("../Utils/functions.js")
-module.exports = async function (message,user) {
+module.exports = async function (message, user) {
     let id = message.author.id;
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
-    return functions.replyMessage(message,'Please come back later')
-    functions.sendMessage(message.channel, {
-      embed: { //displays guild stats
-        title: "Bot Stats",
-        color: 0xF1C40F,
-        fields: [{ //displays variables
-          name: "Total Accounts",
-          value: Object.keys(userData).length,
-          inline: true
-        }, { //displays variables
-          name: "Total Guilds",
-          value: Object.keys(guildData).length,
-          inline: true
-        }, {
-          name: "Total Servers",
-          value: bot.guilds.size,
-          inline: true
-        }]
-      }
+    return Promise.all([client.db("current").collection("userData").count({})]).then(ret => {
+        functions.sendMessage(message.channel, {
+            embed: { //displays guild stats
+                title: "Bot Stats",
+                color: 0xF1C40F,
+                fields: [{ //displays variables
+                    name: "Total Accounts",
+                    value: ret[0],
+                    inline: true
+                }, { //displays variables
+                    name: "Total Guilds",
+                    value: Object.keys(guildData).length,
+                    inline: true
+                }, {
+                    name: "Total Servers",
+                    value: bot.guilds.size,
+                    inline: true
+                }]
+            }
+        });
     });
 }
