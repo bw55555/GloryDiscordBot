a26b89488e9463d4418e12dcc2839fc76372a578
Brian Wang <wanbr@utschools.ca>
2020-01-05 20:59:05 -0500

    Bug fix

--- Commands/marry.js
+++ Commands/marry.js
@@ -20,15 +20,15 @@ module.exports = async function (message,user) {
             functions.MessageAwait(message.channel, target._id, "<@" + target._id + ">, <@" + user._id + "> wishes to marry you! Type `I do` become married!", "I do",
                 function (response, extraArgs) {
                     Promise.all([functions.getUser(extraArgs[0]._id), functions.getUser(extraArgs[1]._id)]).then(ret => {
+                        let user = ret[0]
+                        let target = ret[1]
+                        let message = extraArgs[2]
                         if (target.marry != "None") {
                             return functions.sendMessage(message.channel, "<@" + target._id + "> is already married 😭");
                         }
                         if (user.marry != "None") {
                             return functions.sendMessage(message.channel, "You are already married 😭");
                         }
-                        let user = ret[0]
-                        let target = ret[1]
-                        let message = extraArgs[2]
                         functions.setProp("userData", { _id: user._id }, { $set: { "marry": target._id } })
                         functions.setProp("userData", { _id: target._id }, { $set: { "marry": user._id } })
                         functions.sendMessage(message.channel, "<:nixheart:506240330916429837><:nixheart:506240330916429837><:nixheart:506240330916429837>🎉🎉🎉Congradulations! <@" + target._id + "> and <@" + user._id + "> are now married!🎉🎉🎉<:nixheart:506240330916429837><:nixheart:506240330916429837><:nixheart:506240330916429837>");
