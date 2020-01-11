4bb790958169b7db69fe8f90cdbc79f4937d581a
Brian Wang <wanbr@utschools.ca>
2020-01-10 22:41:55 -0500

    convert eventsummon to mongo

--- Commands/eventsummon.js
+++ Commands/eventsummon.js
@@ -12,10 +12,9 @@ module.exports = async function (message, user) {
     }
     if (time > 0) { functions.replyMessage(message, "Boss will be summoned in " + functions.displayTime(time, 0)) }
     bot.setTimeout(function () {
-        let item = ret[0]
-        mobData[message.channel.id] = {};
-        mobData[message.channel.id].name = 'Fallen Angel'
-        mobData[message.channel.id].url = 'https://i.imgur.com/InedjHz.jpg'
+        let raid = {"_id": message.channel.id};
+        raid.name = 'Fallen Angel'
+        raid.url = 'https://i.imgur.com/InedjHz.jpg'
         if (words.length > 1) {
             let summonlevel = parseInt(words[1])
             if (isNaN(summonlevel)) { return functions.sendMessage(message.channel, "The boss level must be an integer!") }
@@ -25,32 +24,33 @@ module.exports = async function (message, user) {
             if (isNaN(summonatk)) { return functions.sendMessage(message.channel, "The boss atk must be an integer!") }
             let summonreward = parseInt(words[4])
             if (isNaN(summonreward)) { return functions.sendMessage(message.channel, "The boss reward must be an integer!") }
-            mobData[message.channel.id].alive = true;
-            mobData[message.channel.id].raid = true;
-            mobData[message.channel.id].attack = summonatk;
-            //mobData[message.channel.id].defense = summondef;
-            mobData[message.channel.id].currenthealth = summonhp;
-            mobData[message.channel.id].maxhealth = summonhp;
-            mobData[message.channel.id].reward = summonreward;
-            mobData[message.channel.id].level = summonlevel;
-            mobData[message.channel.id].attacklist = [];
-        } else if (words.length == 1) {
+            raid.alive = true;
+            raid.raid = true;
+            raid.attack = summonatk;
+            //raid.defense = summondef;
+            raid.currenthealth = summonhp;
+            raid.maxhealth = summonhp;
+            raid.reward = summonreward;
+            raid.level = summonlevel;
+            raid.attacklist = [];
+        } else {
             //let roll = 1 + ((Math.random() - 0.5) * 0.2);
             let summonlevel = 200 + Math.floor(Math.random() * 100)
-            mobData[message.channel.id].alive = true;
-            mobData[message.channel.id].raid = true;
-            mobData[message.channel.id].attack = Math.floor(summonlevel * 12);
-            //mobData[message.channel.id].defense = summondef;
-            mobData[message.channel.id].currenthealth = Math.floor(summonlevel * 50);
-            mobData[message.channel.id].maxhealth = Math.floor(summonlevel * 50);
-            mobData[message.channel.id].reward = Math.floor(summonlevel * 5000);
-            mobData[message.channel.id].level = summonlevel;
-            //mobData[message.channel.id].itemReward = itemrewardId;
-            mobData[message.channel.id].attacklist = [];
+            raid.alive = true;
+            raid.raid = true;
+            raid.attack = Math.floor(summonlevel * 12);
+            //raid.defense = summondef;
+            raid.currenthealth = Math.floor(summonlevel * 50);
+            raid.maxhealth = Math.floor(summonlevel * 50);
+            raid.reward = Math.floor(summonlevel * 5000);
+            raid.level = summonlevel;
+            //raid.itemReward = itemrewardId;
+            raid.attacklist = [];
         }
+        functions.setObject("mobData", raid)
         message.channel.overwritePermissions(message.guild.roles.get("536599503608872961"), {
             "READ_MESSAGES": true,
             "SEND_MESSAGES": true
-        }).then(function () { functions.replyMessage(message, "Boss summoned. It is level " + mobData[message.channel.id].level + "!\n" + "<@&564565782852272140>"); }).catch(console.error);
+        }).then(function () { functions.replyMessage(message, "Boss summoned. It is level " + raid.level + "!\n" + "<@&564565782852272140>"); }).catch(console.error);
     }, time)
 }
