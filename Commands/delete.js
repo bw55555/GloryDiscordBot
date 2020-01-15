e5315a662de8d42a6a5a701ca17dbfbf28da8f75
Brian Wang <wanbr@utschools.ca>
2020-01-15 11:06:07 -0500

    Fix delete

--- Commands/delete.js
+++ Commands/delete.js
@@ -6,7 +6,7 @@ module.exports = async function (message, user) {
         let id = extraArgs[1]
         let message = extraArgs[0]
         if (user.guild != "None") { return functions.replyMessage(message, "You cannot delete your character if you are in a guild!") }
-        client.db("current").collection("itemData").deleteMany({ "owner": id });
+        functions.deleteObjects("itemData",{ "owner": id });
         if (user.marry != "None") { functions.setProp("userData", { "_id": user.marry }, { $set: { "marry": "None" } }) }
         functions.deleteUser(id);
         functions.replyMessage(message, 'Your character has been deleted. :(')
