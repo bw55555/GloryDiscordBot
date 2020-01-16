e5787de62f601a39df101ace9523a059622532b6
Brian Wang <wanbr@utschools.ca>
2020-01-15 19:59:14 -0500

    Bug fix

--- Commands/worldattack.js
+++ Commands/worldattack.js
@@ -1,6 +1,6 @@
 var functions = require("../Utils/functions.js")
 module.exports = async function (message, user) {
-    return Promise.all([functions.getObject("mobData", message.channel.id)]).then(ret => {
+    return Promise.all([functions.getObject("mobData", "world")]).then(ret => {
         let raid = ret[0]
         functions.raidAttack(message, user, raid, true, false, true)
         functions.setObject("mobData", raid)
