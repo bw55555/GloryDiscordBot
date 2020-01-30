2b237d240ea850585cee0b451b1582fda488b1f1
Brian Wang <wanbr@utschools.ca>
2020-01-29 19:57:16 -0500

    Fix daily

--- Commands/daily.js
+++ Commands/daily.js
@@ -4,7 +4,7 @@ module.exports = async function (message, user) {
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
     if (functions.calcTime(user.cooldowns.daily, ts) < 0) {//Checks if last daily object is L
-        user.cooldowns.daily = ts + 24 * 60 * 60 //Sets daily as L
+        user.cooldowns.daily = ts + 24 * 60 * 60 * 1000//Sets daily as L
         let boxNumber = user.ascension + 1
         user.consum.box += boxNumber; //gives you one box
         functions.sendMessage(message.channel, "You received " + boxNumber + " mysterious box(es)... what could it contain?");
