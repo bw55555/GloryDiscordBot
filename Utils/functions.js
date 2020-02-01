c9934b0e1bb1c292f72b14f1fbab7dda61fd247f
Brian Wang <wanbr@utschools.ca>
2020-01-31 21:13:51 -0500

    Hopefully fix voteitem

--- Utils/functions.js
+++ Utils/functions.js
@@ -972,7 +972,7 @@ async function voteItem(message, user, dm) {
         if (target.glory != undefined && target.glory < 100) {
             target.glory += Math.random() * 0.5;
         }
-        target.consum["box"] += numboxes
+        target.consum.box += numboxes
         sendMessage(message.channel, "<@" + target._id + "> has been given " + numboxes + " boxes!\n" + text)
         if (dm) dmUser(target, "Thank you for voting! You have been given " + numboxes + " boxes!\n" + text)
         setUser(target)
