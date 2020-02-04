68dcf002fc88acf99018343b775b774fa4e006b5
Tim Goudriaan <tgoudriaan@meddex.nl>
2020-02-04 08:50:12 +0100

    Stage1

--- Utils/functions.js
+++ Utils/functions.js
@@ -1225,7 +1225,7 @@ function checkBurn(message,user) {
 function checkAttack(message, user) {
     let ts = message.createdTimestamp;
 
-    console.log("Test 123")
+    console.log(arguments)
 
 }
 
