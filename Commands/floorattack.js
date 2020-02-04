19f3aaaabe105f72c5e5ca21ac1dc51c5b1531be
Tim Goudriaan <tgoudriaan@meddex.nl>
2020-02-04 08:21:24 +0100

    Stage1

--- Commands/floorattack.js
+++ Commands/floorattack.js
@@ -5,10 +5,7 @@ module.exports = async function (message, user) {
 
     return Promise.all([functions.getFloorMob(floor)]).then(ret => {
 
-    let _id = 0;
     let mobinfo = ret[0]
-    let damage = functions.calcDamage(message, user, user);
-    let counter = functions.calcDamage(message, user, user);
 
     functions.sendMessage(message.channel, {
         embed: {
@@ -23,16 +20,6 @@ module.exports = async function (message, user) {
                 {
                     name: "Level",
                     value: mobinfo.bossId,
-                    inline: true
-                },
-                {
-                    name: "Damage",
-                    value: damage,
-                    inline: true
-                },
-                {
-                    name: "Counter",
-                    value: counter,
                     inline: true
                 }
             ]
