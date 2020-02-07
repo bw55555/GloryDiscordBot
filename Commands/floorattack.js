89f2989df8ec9bcfa7214fbfa789e4367f8cc267
Tim Goudriaan <tgoudriaan@meddex.nl>
2020-02-07 07:56:20 +0100

    Add Test

--- Commands/floorattack.js
+++ Commands/floorattack.js
@@ -1,16 +1,14 @@
-module.exports = async
-function (message, user, guild) {
+module.exports = async function (message, user, guild) {
     let id = message.author.id;
     let ts = message.createdTimestamp;
     let floor = user.floorlevel;
 
-    return Promise.all([functions.getFloorMob(floor)]).then(ret = > {
+    return Promise.all([functions.getFloorMob(floor)]).then(ret => {
 
-            let mobinfo = ret[0]
-            functions.floorAttack(message, user, guild, mobinfo);
+    let mobinfo = ret[0]
+
+    functions.floorAttack(message, user, guild);
 
 })
-    if (devs.indexOf(id) == -1) {
-        return
-    }
+    if (devs.indexOf(id) == -1) {return}
 }
