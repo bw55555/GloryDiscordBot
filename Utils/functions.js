6198067527519c472aa5c91b82d65a31a8a79524
Tim Goudriaan <tgoudriaan@meddex.nl>
2020-02-07 07:59:56 +0100

    Add Test

--- Utils/functions.js
+++ Utils/functions.js
@@ -1272,7 +1272,7 @@ function floorAttack(message, user, guild, floorMobInfo) {
       let vAttack = 20;
 
       let debug1 = calcFloorMobDef(message, vAttack);
-      replyMessage(message, floorMobInfo)
+      replyMessage(message, floorMobInfo.attack)
 
 
 }
