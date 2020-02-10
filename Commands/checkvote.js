28131bac235529bc24e7c7c0f12f5a375ae5ec9b
Tim Goudriaan <tgoudriaan@meddex.nl>
2020-02-10 14:21:32 +0100

    Add Test

--- Commands/checkvote.js
+++ Commands/checkvote.js
@@ -8,8 +8,8 @@ module.exports = async function (message, user) {
         path: '/api/bots/536622022709608468/check?userId=444564799913721876',
         method: 'GET',
         headers: {
-            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzNjYyMjAyMjcwOTYwODQ2OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ5ODE3MTkxfQ.2pFz9ECHEzpi0OtneZ2LrP-_apXf5oXj2Tsv_OaUPTw',
-            'content-type' 'application/json'
+            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzNjYyMjAyMjcwOTYwODQ2OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ5ODE3MTkxfQ.2pFz9ECHEzpi0OtneZ2LrP-_apXf5oXj2Tsv_OaUPTw'
+
         }
     };
 
