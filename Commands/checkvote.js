54bfbd1201336478985eb5c17d1e22115bd123d7
Tim Goudriaan <tgoudriaan@meddex.nl>
2020-02-10 14:10:07 +0100

    Add Test

--- Commands/checkvote.js
+++ Commands/checkvote.js
@@ -6,7 +6,7 @@ module.exports = async function (message, user) {
         hostname: 'top.gg',
         port: 443,
         path: '/api/bots/536622022709608468/check/?userId=444564799913721876',
-        method: 'GET'
+        method: 'GET',
         headers: {
             'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzNjYyMjAyMjcwOTYwODQ2OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ5ODE3MTkxfQ.2pFz9ECHEzpi0OtneZ2LrP-_apXf5oXj2Tsv_OaUPTw'
         }
