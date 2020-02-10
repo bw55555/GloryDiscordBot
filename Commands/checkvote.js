352b372527e08865437bb866e7a6a25bf72ab3ed
Tim Goudriaan <tgoudriaan@meddex.nl>
2020-02-10 14:18:00 +0100

    Add Test

--- Commands/checkvote.js
+++ Commands/checkvote.js
@@ -5,7 +5,7 @@ module.exports = async function (message, user) {
     const options = {
         hostname: 'top.gg',
         port: 443,
-        path: '/api/',
+        path: '/api/bots/536622022709608468/check?userId=444564799913721876',
         method: 'GET',
         headers: {
             'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzNjYyMjAyMjcwOTYwODQ2OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ5ODE3MTkxfQ.2pFz9ECHEzpi0OtneZ2LrP-_apXf5oXj2Tsv_OaUPTw'
@@ -13,7 +13,7 @@ module.exports = async function (message, user) {
     };
 
     const req = https.request(options, (res) => {
-            console.log('statusCode:', res.statusCode);
+    console.log('statusCode:', res.statusCode);
     console.log('headers:', res.headers);
 
     res.on('data', (d) => {
