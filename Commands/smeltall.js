651066993dd55d74f29e56ddf9581e4d287d0157
Brian Wang <wanbr@utschools.ca>
2020-02-10 20:46:57 -0500

    Revert "Fix smeltall? Hopefully"
    This reverts commit 8a1306c98cc9216277a156c294999a0b01338ede.

--- Commands/smeltall.js
+++ Commands/smeltall.js
@@ -4,9 +4,9 @@ module.exports = async function (message, user) {
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
     if (functions.calcTime(user.cooldowns.smeltall, ts) > 0) { return functions.replyMessage(message, "Be patient! Wait " + functions.displayTime(user.cooldowns.smeltall, ts)) }
-    let prevmats = user.materials
-    let prevmoney = user.money
-    let prevxp = user.xp
+    let totalmaterials = 0
+    let totalmoney = 0
+    let totalxp = 0
     if (Object.keys(user.inventory).length == 0) { return functions.sendMessage(message.channel, "There's nothing in your inventory that matches the selected filters... ") }
     functions.setCD(user, ts, smeltallcd * 60, "smeltall")
     return functions.itemFilter(message, user, { "fav": false }).then(smeltItems => {
@@ -17,11 +17,17 @@ module.exports = async function (message, user) {
             let weapon = smeltItems[i]
             if (weapon.rarity == "Unique" || weapon.rarity == 9 || user.weapon._id == weapon._id) { continue }
             delarr.push(weapon._id)
-            functions.smeltItem(user, weapon, true, true)
+            let itemRewards = functions.smeltItem(user, weapon, true, true)
+            if (isNaN(itemRewards[0]) || isNaN(itemRewards[1]) || isNaN(itemRewards[2])) {
+                continue
+            }
+            totalxp += itemRewards[0]
+            totalmoney += itemRewards[1]
+            totalmaterials += itemRewards[2]
             count += 1
         }
         functions.deleteObjects("itemData", { "_id": {$in: delarr}})
         if (count == 0) { return functions.replyMessage(message, "You do not have any items to smelt!") }
-        functions.sendMessage(message.channel, "You have smelted " + count + " items for " + user.materials-prevmats + " materials, $" + user.money - prevmoney + ", and " + user.xp-prevxp + " xp.")
+        functions.sendMessage(message.channel, "You have smelted " + count + " items for " + totalmaterials + " materials, $" + totalmoney + ", and " + totalxp + " xp.")
     })
 }
