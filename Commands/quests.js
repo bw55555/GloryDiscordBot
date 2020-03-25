4eee780a2c06c2da4eece80c1b45a91ac46d7bf4
Brian Wang <wanbr@utschools.ca>
2020-03-24 20:04:37 -0400

    bug fix

--- Commands/quests.js
+++ Commands/quests.js
@@ -5,7 +5,7 @@ module.exports = async function (message, user) {
     let words = message.content.trim().split(/\s+/)
     if (user.quests.length == 0) { return functions.replyMessage(message, "You have no quests!") }
     let fields = []
-    for (var i = 0; i < users.quests.length; i++) {
+    for (var i = 0; i < user.quests.length; i++) {
         fields.push({
             name: user.quests[i].name,
             value: "**Condition:** " + user.quests[i].condition.category+" ("+user.quests[i].current+"/"+user.quests[i].total+")\n**Reward:** "+JSON.stringify(user.quests[i].reward),
