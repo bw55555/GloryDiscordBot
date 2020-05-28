362dc8d90fc1f31acc68ba449ee21efae5b02685
Brian Wang <wanbr@utschools.ca>
2020-05-27 23:18:47 -0400

    Bug fix

--- Utils/MessageAwait.js
+++ Utils/MessageAwait.js
@@ -20,7 +20,7 @@ async function MessageAwait(channel, userid, initialTextToSend, compareFunc, onS
         }
     }
     if (onSuccess == undefined || onSuccess == null) { onSuccess = function (response) { return response } }
-    if (waitList[userid] +30000 > Date.now()) { return sendMessage(channel, "<@"+userid+"> already has a message awaiting confirmation.")}
+    if (waitList[userid] > Date.now()+30000) { return sendMessage(channel, "<@"+userid+"> already has a message awaiting confirmation.")}
     waitList[userid] = Date.now();
     if (channel.type == "dm" || channel.type == "group" || channel.memberPermissions(bot.user) == null || channel.memberPermissions(bot.user).has("SEND_MESSAGES")) {
         return channel.send(initialTextToSend).then(() => {
