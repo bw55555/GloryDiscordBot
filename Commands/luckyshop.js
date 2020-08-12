910d753c1b255309cd3ad8df5a849ad249e32593
Brian Wang <wanbr@utschools.ca>
2020-08-12 16:46:39 -0400

    add luckyshop

---
+++ Commands/luckyshop.js
@@ -0,0 +1,75 @@
+let storeitems = [
+    { "type": "boxes", "min": 10, "max": 200, "price": 125000, "chance": 1000000 },
+    { "type": "reroll", "min": 1, "max": 1, "price": 20000000, "chance": 100 },
+    { "type": "sp", "min": 1, "max": 1, "price": 500000000, "chance":1 },
+    { "type": "materials", "min": 1000, "max": 20000, "price": 10000, "chance": 500000 },
+    { "type": "crystals", "min": 10, "max": 100, "price": 100000, "chance": 50000}
+]
+module.exports = async function (message, user) {
+    let id = message.author.id;
+    let ts = message.createdTimestamp;
+    let words = message.content.split(/\s+/)
+    let scmd = words[1];
+    if (user.luckyshop == undefined) { user.luckyshop = [];}
+    if (scmd == "buy") {
+        let item = parseInt(words[2]);
+        if (isNaN(item) || item < 0 || item > user.luckyshop.length) {
+            return functions.replyMessage(message, "This item is not defined")
+        }
+        let temp = user.luckyshop[item].split(/\s+/)
+        let type = storeitems[temp[0]].type;
+        let amt = parseInt(temp[1]);
+        let unitprice = parseInt(temp[2]);
+        let totalprice = unitprice * amt;
+        if (user.money < totalprice) {
+            return functions.replyMessage(message, "You do not have enough money to buy this!")
+        }
+        
+        if (type == "boxes") {
+            user.consum.box += amt;
+        } else if (type == "reroll") {
+            user.consum.reroll += amt;
+        } else if (type == "sp") {
+            user.consum.sp += amt;
+        } else if (type == "materials") {
+            user.materials += amt;
+        } else if (type == "crystals") {
+            functions.getObject("guildData", user.guild).then(guild => { guild.crystals += amt; functions.setObject("guildData", guild) });
+        } else {
+            return functions.replyMessage(message, "There was an error. Please report it in the support server. The link is in !info")
+        }
+        user.money -= totalprice;
+        user.luckyshop.splice(item, 1)
+        functions.replyMessage(message, "You have bought "+amt+ " "+ type + " for $"+totalprice)
+    }
+    else if (scmd == "refresh") {
+        let totalchance = storeitems.reduce((tot, next) => tot + next.chance)
+        let randomchance = Math.floor(Math.random() * totalchance);
+        let currtot = 0;
+        user.luckyshop = [];
+        for (let item = 0; item < 4; item++) {
+            for (let i = 0; i < storeitems.length; i++) {
+                currtot += storeitems[i].chance;
+                if (randomchance < currtot) {
+                    let amt = storeitems[i].min + Math.floor(Math.pow(Math.random(), 4) * (storeitems[i].max - storeitems[i].min + 1))
+                    let price = storeitems[i].price * 0.5 * Math.pow(Math.random(), 4)
+                    if (user.vip != undefined) {price *= (1-user.vip.level/10)}
+                    user.luckyshop.push(i + " "+amt +" "+price)
+                    break;
+                }
+            }
+        }
+    }
+    else {
+        if (user.luckyshop.length == 0) { return functions.replyMessage(message) }
+        let fields = [];
+        for (let i = 0; i < user.luckyshop.length; i++) {
+            let item = user.luckyshop[i].split(" ");
+            fields.push({
+                name: storeitems[item[0]].type + " ("+item[1]+")",
+                value: "**Price**: " + item[1]*item[2] + " (~~"+storeitems[item[0]].price+"~~ "+item[2]+" ea)",
+                inline: false,
+            })
+        }
+    }
+}
--- Commands/setvip.js
+++ Commands/setvip.js
@@ -5,10 +5,10 @@ module.exports = async function (message, user) {
     if (message.guild.id != "536599503608872961") { return functions.replyMessage(message, "You cannot do this outside the support server!") }
     let viproles = ["560322482586910722", "560325910855811120", "560323135291785216", "560323708321792003"];
     let vipbenefits = [
-        { "lucky": 0.05, "extrarerolls": 0 },
-        { "lucky": 0.1, "extrarerolls": 1 },
-        { "lucky": 0.2, "extrarerolls": 2 },
-        { "lucky": 0.5, "extrarerolls": 3 },
+        { "level": 1, "lucky": 0.05, "extrarerolls": 0 },
+        { "level": 2, "lucky": 0.1, "extrarerolls": 1 },
+        { "level": 3, "lucky": 0.2, "extrarerolls": 2 },
+        { "level": 4, "lucky": 0.5, "extrarerolls": 3 },
     ]
     let viplevel = parseInt(words[1]);
     for (var i = viproles.length - 1; i >= 0; i--) {
@@ -17,6 +17,7 @@ module.exports = async function (message, user) {
             if (user.vip.rerollsclaimed == undefined) { user.vip.rerollsclaimed = 0;}
             user.vip.lucky = vipbenefits[i].lucky;
             user.vip.extrarerolls = vipbenefits[i].extrarerolls;
+            user.vip.level = vipbenefits[i].level;
             functions.replyMessage(message, "Your vip level was set to "+ i)
             break;
         }
