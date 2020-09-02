ebf4f634c9fb8030715745cec1e110ecf01286e7
Brian Wang <wanbr@utschools.ca>
2020-09-02 13:08:36 -0400

    explosions are enabled

--- Commands/explosion.js
+++ Commands/explosion.js
@@ -3,10 +3,13 @@ module.exports = async function (message, user) {
     let id = message.author.id;
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
-    return functions.replyMessage(message, "Explosions are disabled. ")
+    //return functions.replyMessage(message, "Explosions are disabled. ")
     if (user.consum.explosion <= 0) { return functions.replyMessage(message, "You don't have any bombs left!") }
+    if (user.dead) { return functions.replyMessage(message, "You can't throw a bomb while dead!") }
+    if (user.money <= 10000000) { return functions.replyMessage(message, "You need $10000000 to be able to throw a bomb!")}
     let chance = Math.random();
-    if (chance < 0.25) { user.currenthealth -= 10000; return functions.replyMessage(message, "The bomb malfunctioned and exploded in your hands... ") }
+    user.consum.explosion -= 1;
+    if (chance < 0.25) { user.currenthealth -= 10000; return functions.replyMessage(message, "The bomb malfunctioned and exploded in your hands... "); }
     else if (chance < 0.5) { return functions.replyMessage(message, "The bomb malfunctioned and refused to explode...") }
     else {
         let inchannel = talkedRecently[message.channel.id];
@@ -16,6 +19,8 @@ module.exports = async function (message, user) {
                 nearby.push(person)
             }
         }
+
+        user.money -= 10000000
         if (nearby.length == 0) { return functions.replyMessage(message, "The bomb exploded, but no one got hit... ") }
         functions.findUsers({ "_id": { $in: nearby }, "dead": false }).then(ret => {
             let text = "https://tenor.com/view/explosion-gif-9488133 \n";
