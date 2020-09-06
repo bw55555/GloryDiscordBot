284cd4a0e91c2e070e218faeb8e9af5b36385b72
Brian Wang <wanbr@utschools.ca>
2020-09-06 10:54:01 -0400

    fix aconsum

--- Commands/adminconsum.js
+++ Commands/adminconsum.js
@@ -6,12 +6,13 @@ module.exports = async function (message, user) {
     if (admins.indexOf(id) == -1) { return user}
     if (words.length != 4) { functions.sendMessage(message.channel, "!adminconsum [target] [item] [amount]");return user;}
 
-    return Promise.all([functions.validate(message,user)]).then(ret => {
+    return Promise.all([functions.validate(message, user)]).then(ret => {
         let target = ret[0];
-        if(target == false) { return user; }
+        if (target == false) { return user; }
         var item = words[2]
         var amount = parseInt(words[3]);
-        if (isNaN(amount)) { functions.sendMessage(message.channel, "!adminconsum [target] [item] [amount]");return user; }
+        if (isNaN(amount)) { return functions.sendMessage(message.channel, "The amount must be an integer!"); }
+        if (item == undefined || target.consum[item] == undefined) { return functions.sendMessage(message.channel, "This item is not defined!")}
         target.consum[item] += amount;
         functions.sendMessage(message.channel, "<@" + target._id + "> given " + amount + " " + item);
         functions.setUser(target);
