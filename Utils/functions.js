bd52a7301fe5672c22d814887769c556e496a037
Brian Wang <wanbr@utschools.ca>
2020-09-07 15:53:43 -0400

    hotfix

--- Utils/functions.js
+++ Utils/functions.js
@@ -1721,9 +1721,9 @@ function adminQuest(text, target) {
         if (isNaN(value)) { return "Please enter a correct quest reward. The amount of property " + key + " must be an integer." }
         reward[key] = value;
     }
-    makeQuest(target, name, flavortext, conditions, reward, type, mqid);
+    makeQuest(target, name, flavortext, conditions, reward, mqid);
     setUser(target);
-    return "The quest, `"+name+"` was assigned! Use `!quests` to see it!"
+    return "The quest `"+name+"` was assigned! Use `!quests` to see it!"
 }
 
 function isCD(user, ts, cdtype) {
