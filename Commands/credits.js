2b118c22c3c9c394862b57b8ca90f74535c0c54f
Brian Wang <wanbr@utschools.ca>
2020-09-06 20:26:26 -0400

    update credits

--- Commands/credits.js
+++ Commands/credits.js
@@ -23,15 +23,11 @@ module.exports = async function (message, user) {
             "fields": [
                 {
                     "name": "Developers",
-                    "value": "Nix#6340 (Kinda just here)\nbw55555#5977 (Hosting and Digitechnics)\nDosei#4141 (Backing Up and Doublehosting)\n3628800#4251 (Artwork and Probability and Crashing Things)\nRazoreign#6576 (Just ignore me)"
+                    "value": "Nix#6340 (Kinda just here)\nbw55555#5977 (Hosting and Digitechnics)\nDosei#4141 (Backing Up and Doublehosting)\n3628800#4251 (Artwork and Probability and Crashing Things)\nRazoreign#6576 (Just ignore me)\nSpooky#0001 (Database and Network Troubleshooting))"
                 },
                 {
                     "name": "Administrators",
-                    "value": "AgendBro#7035 (Eurasia Admin)\nReaper#6365 (Americas Admin)"
-                },
-                {
-                    "name": "Moderators",
-                    "value": "R3D#4836\nZev#6471\norb#5588\nRytoBuryto#4525\nCorbyn#4362\nYoku#4505"
+                    "value": "Pheenix#7022 \nReaper#6365 \nSlay#0622"
                 },
                 {
                     "name": "Other Helpful People",
