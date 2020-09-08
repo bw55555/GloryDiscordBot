f96ebbcaf0b2f386444ac2c4f82251b29bc9bc4d
Brian Wang <wanbr@utschools.ca>
2020-09-07 21:59:24 -0400

    remove devlock

--- Commands/runes.js
+++ Commands/runes.js
@@ -3,7 +3,6 @@ module.exports = async function (message, user) {
     let id = message.author.id;
     let ts = message.createdTimestamp;
     let words = message.content.split(/\s+/)
-    if (admins.indexOf(id) == -1) { return functions.replyMessage(message, "This feature is currently under development!") }
     if (user.runes == undefined) { user.runes = [0, 0, 0, 0, 0, 0, 0] }
     while (user.runes.length < 7) { user.runes.push(0) }
     let text = "```\nYour Runes: \n";
