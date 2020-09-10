6ad3eecd1a572efc9aed1a38b17bf93dee7762fb
Brian Wang <wanbr@utschools.ca>
2020-09-10 18:23:18 -0400

    adminlock adminquest

--- Commands/adminquest.js
+++ Commands/adminquest.js
@@ -3,6 +3,7 @@ module.exports = async function (message, user) {
     let id = message.author.id;
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
+    if (admins.indexOf(id) == -1) { return }
     return Promise.all([functions.validate(message, user)]).then(ret => {
         let target = ret[0];
         if (target == false) { return; }
