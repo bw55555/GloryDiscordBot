536e8dbcc577daa037f19c12caba98c08de06ab9
Brian Wang <wanbr@utschools.ca>
2020-09-10 16:51:41 -0400

    fix everyone bug

--- Utils/functions.js
+++ Utils/functions.js
@@ -121,14 +121,8 @@ function sendMessage(channel, text, override) {
     override = (override == true) ? true : false
     if (!override && channel.guild != undefined && serverData[channel.guild.id] != undefined && serverData[channel.guild.id].disabledChannels.indexOf(channel.id) != -1) { return; }
     if (channel.type != "dm" && channel.type != "group" && (channel.memberPermissions(bot.user) != null && !channel.memberPermissions(bot.user).has("SEND_MESSAGES"))) { return }
-    if (text.indexOf("@everyone") != -1) {
-        if (message.guild.id != devData.debugGuildId) {
-            text = "Don't be sneaky and try to ping everyone!"
-            return false;
-        } else {
-            text = "Don't be sneaky and try to ping everyone!"
-            return false;
-        }
+    while (text.indexOf("@everyone") != -1) {
+        text.replace("@everyone", "everyone")
     }
     return channel.send(text).catch(function (err) {
         
