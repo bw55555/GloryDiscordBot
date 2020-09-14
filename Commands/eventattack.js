c900f6cfef479841944983a7f82168056062fccf
Brian Wang <wanbr@utschools.ca>
2020-09-14 17:47:28 -0400

    hotfix

--- Commands/eventattack.js
+++ Commands/eventattack.js
@@ -1,7 +1,12 @@
 
 module.exports = async function (message, user) {
+    if (message.channel.id != devData.eventRaidChannel) { return;}
     return Promise.all([functions.getObject("mobData", "event")]).then(ret => {
         let raid = ret[0]
+        if (raid == false) {
+            functions.sendMessage(message.channel, "There is no event raid going on!");
+            return;
+        }
         functions.raidAttack(message, user, raid, "event")
         functions.setObject("mobData", raid)
     })
