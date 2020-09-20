36a898065e4f73a7c118b042eb695fc3e9cfe5bc
Brian Wang <wanbr@utschools.ca>
2020-09-19 20:49:18 -0400

    hotfix

--- Utils/functions.js
+++ Utils/functions.js
@@ -122,7 +122,7 @@ function sendMessage(channel, text, override) {
     override = (override == true) ? true : false
     if (!override && channel.guild != undefined && serverData[channel.guild.id] != undefined && serverData[channel.guild.id].disabledChannels.indexOf(channel.id) != -1) { return; }
     if (channel.type != "dm" && channel.type != "group" && (channel.permissionsFor(bot.user) != null && !channel.permissionsFor(bot.user).has("SEND_MESSAGES"))) { return }
-    if (text.embed != undefined && channel.type != "dm" && channel.type != "group" && (channel.permissionsFor(bot.user) != null && !channel.permissionsFor(bot.user).has("SEND_EMBEDS"))) { text = "Uh Oh! The bot does not have permission to send embers here!" }
+    if (text.embed != undefined && channel.type != "dm" && channel.type != "group" && (channel.permissionsFor(bot.user) != null && !channel.permissionsFor(bot.user).has("EMBED_LINKS"))) { text = "Uh Oh! The bot does not have permission to send embers here!" }
     while (text.indexOf != undefined && text.indexOf("@everyone") != -1) {
         text.replace("@everyone", "everyone")
     }
