091af452eb819f80bf5277916420bfca633c9607
Brian Wang <wanbr@utschools.ca>
2020-09-20 18:03:15 -0400

    change death penalty of 10% of xp for current level instead of all xp

--- Commands/attack.js
+++ Commands/attack.js
@@ -101,8 +101,14 @@ module.exports = async function (message, user) {
             target.currenthealth = 0;
             user.dead = true;
             user.currenthealth = 0;
-            user.xp = 0
-            target.xp = 0
+            let xplost = Math.floor(functions.checkxp(user) / 10)
+            user.xp -= xplost;
+            if (user.level == 0) { user.xp = 0 }
+            if (user.xp < 0) { user.level -= 1; user.xp += functions.checkxp(user) }
+            xplost = Math.floor(functions.checkxp(target) / 10)
+            target.xp -= xplost;
+            if (target.level == 0) { target.xp = 0 }
+            if (target.xp < 0) { target.level -= 1; target.xp += functions.checkxp(target) }
             target.money -= stolen;
             user.money -= counterstolen;
             functions.sendMessage(message.channel, '<@' + user._id + '> and <@' + target._id + '> was killed! Both lost 10% of their money.');
@@ -131,7 +137,10 @@ module.exports = async function (message, user) {
                 target.bounty = 0;
             }
 
-            target.xp = 0
+            let xplost = Math.floor(functions.checkxp(target) / 10)
+            target.xp -= xplost;
+            if (target.level == 0) { target.xp = 0 }
+            if (target.xp < 0) { target.level -= 1; target.xp += functions.checkxp(target) }
 
             if (user.glory != undefined && target.glory != undefined) {
                 let glorywon = target.glory * 0.001
@@ -159,7 +168,10 @@ module.exports = async function (message, user) {
                 target.money += user.bounty;
                 user.bounty = 0;
             }
-            user.xp = 0
+            let xplost = Math.floor(functions.checkxp(user) / 10)
+            user.xp -= xplost;
+            if (user.level == 0) { user.xp = 0 }
+            if (user.xp < 0) { user.level -= 1; user.xp += functions.checkxp(user) }
 
             if (user.glory != undefined && target.glory != undefined) {
                 let glorywon = user.glory * 0.001
