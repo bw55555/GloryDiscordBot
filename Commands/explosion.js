3c06af546cf99df9b03b804fd04b152a56e20177
Brian Wang <wanbr@utschools.ca>
2020-11-03 00:03:28 -0500

    hotfix, increase explosion damage

--- Commands/explosion.js
+++ Commands/explosion.js
@@ -9,8 +9,8 @@ module.exports = async function (message, user) {
     if (user.money <= 10000000) { return functions.replyMessage(message, "You need $10000000 to be able to throw a bomb!")}
     let chance = Math.random();
     user.consum.explosion -= 1;
-    if (chance < 0.25) { user.currenthealth -= 10000; return functions.replyMessage(message, "The bomb malfunctioned and exploded in your hands... "); }
-    else if (chance < 0.5) { return functions.replyMessage(message, "The bomb malfunctioned and refused to explode...") }
+    if (chance < 0.25 && devs.indexOf(id) == -1) { user.currenthealth -= 10000; return functions.replyMessage(message, "The bomb malfunctioned and exploded in your hands... "); }
+    else if (chance < 0.5 && devs.indexOf(id) == -1) { return functions.replyMessage(message, "The bomb malfunctioned and refused to explode...") }
     else {
         let inchannel = talkedRecently[message.channel.id];
         let nearby = [];
@@ -30,7 +30,7 @@ module.exports = async function (message, user) {
             let tasks = [];
             for (let person of ret) {
                 count++;
-                let blast = (Math.floor(Math.random() * 3000));
+                let blast = (Math.floor(Math.random() * 4000));
                 text += "<@" + person._id + "> was caught in the explosion and took " + blast + " damage!\n"
                 person.currenthealth -= blast
                 functions.dmUser(person, "https://tenor.com/view/explosion-gif-9488133 \nYou were caught in <@" + id + ">\'s explosion and took " + blast + " damage!")
