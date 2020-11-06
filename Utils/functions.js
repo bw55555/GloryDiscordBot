cbac21257e28c2729ffc1d992afb3fbe71ffe360
Brian Wang <wanbr@utschools.ca>
2020-11-06 09:19:46 -0500

    hotfix

--- Utils/functions.js
+++ Utils/functions.js
@@ -186,7 +186,7 @@ function logCommand(message, title, extratext) {
     let mcontent = message.content
     if (mcontent.length >= 500) { mcontent = mcontent.slice(0, 500) + " ..." + (mcontent.length - 500) + " chars not shown" }
     let cid = debugChannelId
-    if (title == "MACRO" || title == "FLAG") { cid == devData.flagChannelId}
+    if (title == "MACRO" || title == "FLAG") { cid =devData.flagChannelId}
     sendMessage(bot.guilds.cache.get(debugGuildId).channels.cache.get(cid), clean(title + "|"+message.author.id + "|" + mcontent + "|" + message.createdTimestamp + extratext))
 }
 
