15fa0fb4b2ca187a4cde6dd349f3f382fe6183c8
Brian Wang <wanbr@utschools.ca>
2020-11-10 21:34:59 -0500

    update tutorial link

--- Commands/tutorial.js
+++ Commands/tutorial.js
@@ -3,5 +3,5 @@ module.exports = async function (message, user) {
     let id = message.author.id;
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
-    functions.sendMessage(message.channel, "The tutorial can be found at https://docs.google.com/document/d/1OtNO65uXrREcPiBi8Bn59yYDci4w2hwcSrRrGID5HAY/edit")
+    functions.sendMessage(message.channel, "The tutorial can be found at https://docs.google.com/document/d/1dbv3F-7LouTyrXtBiVVNc5NIIXc-t_A4qpMkYzOhdmQ/edit?usp=sharing")
 }
