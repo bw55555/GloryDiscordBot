4b219455529dd01fefbac0d5c608cb41a0d61c18
Brian Wang <wanbr@utschools.ca>
2020-11-11 14:40:52 -0500

    hotfix

--- Utils/functions.js
+++ Utils/functions.js
@@ -634,7 +634,7 @@ function calcDamage(message, attacker, defender, initiator, astatus, dstatus) {
     if (x < 0) { truedamage = 0 }
     else {
         if (defense <= 0) {
-            x *= (1 - defense / 50)
+            truedamage = x*(1 - defense / 50)
         }
         if (defmult * defense > x) {
             truedamage = Math.floor(x * Math.pow(1 - (Math.sqrt(defmult * defmult * defense * defense - x * x) / (defmult * defense)), (1 / 3)))
