3e9a4549c5602c2940342cfeb0274e99cface1aa
Brian Wang <wanbr@utschools.ca>
2020-11-16 15:25:18 -0500

    add familiar command

---
+++ Commands/familiar.js
@@ -0,0 +1,34 @@
+
+module.exports = async function (message, user) {
+    let id = message.author.id;
+    let ts = message.createdTimestamp;
+    let words = message.content.split(/\s+/)
+    let word2 = words[1];
+    if (word2 == undefined) { word2 = "" }
+    word2 = word2.toLowerCase();
+    if (word2 == `help`) {
+        /*
+        let page = {
+            "embed": {
+                "color": 0x458B00,
+                "fields": [
+                    {
+                        "name": "The Auction House",
+                        "value": "- List auction items with `!auction list`.\n"
+                            + "- Bid on items from the auction with `!auction bid [itemid] [price]`\n"
+                    }
+                ],
+                "footer": {
+                    "text": "Items bid through the auction will not be refunded under any circumstances."
+                }
+            }
+        }
+        functions.sendMessage(message.channel, page)
+        return
+        */
+    }
+    if (word2 == "list") {
+
+    }
+    
+}
