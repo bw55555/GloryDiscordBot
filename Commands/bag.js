d9dbdf8dd66e2dc4646b05c99ea3658d12c1ddaf
Brian Wang <wanbr@utschools.ca>
2020-12-17 19:13:25 -0500

    hotfix

--- Commands/bag.js
+++ Commands/bag.js
@@ -3,12 +3,12 @@ module.exports = async function (message, user) {
     let id = message.author.id;
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
-    let text = "Your bag: "
+    let text = "Your bag: \n"
     for (let bagitem in user.bag) {
         let arr = bagitem.split("_")
         let bagitemid = arr[0]
         arr.splice(0, 1)
-        itemname = bagItems[bagitemid].name + " "+arr.map((x,i) => x == "" ? "" : "("+bagItems[bagitemid].sub[i][x]+")").join(" ")
+        itemname = bagItems[bagitemid].name + " " + arr.map((x, i) => bagItems[bagitemid].sub[i][x] == "" ? "" : "("+bagItems[bagitemid].sub[i][x]+")").join(" ")
         text += "**" +itemname + "**: "+user.bag[bagitem]+"\n"
     }
     functions.replyMessage(message, text)
