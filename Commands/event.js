ec9237cb1c305c0c5d88d809482d5666fcb2d3c9
Brian Wang <wanbr@utschools.ca>
2020-12-22 21:04:08 -0500

    hotfix

--- Commands/event.js
+++ Commands/event.js
@@ -71,12 +71,12 @@ function refreshQuest(id) {
     let rwd = 1;
     if (amt >= currencies[currency].min * 2) {rwd += 1}
     let conditions = []
-    conditions.push(functions.addQuestCondition("donate", "=>", "Donate " + amt + " " + currencies[currency].name, amt, { "currency": { "value": currency, "operator": "=" } }, "a"))
+    conditions.push(functions.addQuestCondition("donate", ">=", "Donate " + amt + " " + currencies[currency].name, amt, { "currency": { "value": currency, "operator": "=" } }, "a"))
     let mkey = functions.getRandomArrayElement([0, 1, 2], [0.4, 0.4, 0.2]) + 3 * functions.getRandomArrayElement([0, 1, 2, 3, 4, 5, 6, 7], [0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05])
     rwd += Math.floor(mkey/9)
     let mname = monsters[mkey]
     let mamt = Math.floor((1 + Math.random()) * 3 * ((mkey + 1) % 3) + 1)
-    conditions.push(functions.addQuestCondition("raidAttack", "=>", "Kill " + mamt + " " + mname, mamt, { "raid.currenthealth": { "value": 0, "operator": "<=" }, "raid.name": { "value": mname, "operator": "=" } }, "a"))
+    conditions.push(functions.addQuestCondition("raidAttack", ">=", "Kill " + mamt + " " + mname, mamt, { "raid.currenthealth": { "value": 0, "operator": "<=" }, "raid.name": { "value": mname, "operator": "=" } }, "a"))
     let exq = functions.makeQuest(undefined, "Help us Decorate!", undefined, conditions, { "present": rwd })
     exq.event = true;
     exq._id = id
