7d23c6fc1fb829f6425643c27ee30f82e3691d3b
Brian Wang <wanbr@utschools.ca>
2021-08-01 13:35:57 -0400

    hotfix

--- Commands/setmaintenancetext.js
+++ Commands/setmaintenancetext.js
@@ -3,12 +3,12 @@ module.exports = async function (message, user) {
     let ts = message.createdTimestamp;
     let words = message.content.split(/\s+/)
     if (devs.indexOf(id) == -1) { return }
-    if (devData.enable) { return functions.replyMessage(message, "You cannot do this while the bot is enabled!") }
+    //if (devData.enable) { return functions.replyMessage(message, "You cannot do this while the bot is enabled!") }
     functions.MessageAwait(message.channel, id, "Enter the new maintenance text.", (x) => true, async function (response, extraArgs) {
         let message = extraArgs[0];
-        devData.maintenancetext = response
+        devData.maintenancetext = response.content
         functions.setObject("devData", devData)
         //await functions.deleteObjects("mobData", {})
-        functions.replyMessage(message, response)
+        functions.replyMessage(message, response.content)
     }, [message])
 }
