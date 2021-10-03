db4ae6a86934a4eb7fd67d2db92a644b942d5045
bw55555 <brianwang2013@gmail.com>
2021-10-02 23:34:32 -0400

    hotfix swap not working for vampires

--- Commands/swap.js
+++ Commands/swap.js
@@ -3,14 +3,12 @@ module.exports = async function (message, user) {
     let id = message.author.id;
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
-    if (user.triangleid == 11) {
-        user.triangleid = 14;
-        user.trianglemod = 1.4; //This top part turns you into a bloodweaver, setting the mod to 1.7
+    if (user.triangleid % 300 == 11) {
+        user.triangleid += 3;
         functions.replyMessage(message, 'You are now a Bloodweaver! You sacrifice life to deal more damage!');
     }
-    else if (user.triangleid == 14) {
-        user.triangleid = 11;
-        user.trianglemod = 1.4;//turns you into a lifeweaver, setting the mod to 1.4
+    else if (user.triangleid % 300 == 14) {
+        user.triangleid -= 3;
         functions.replyMessage(message, 'You are now a Lifeweaver! Steal some of your opponent\'s life whenever you damage them!');
     }
 }
