3a3c5b3a8d6ce3f489ce9c5431cafe5b466e05c6
bw55555 <brianwang2013@gmail.com>
2021-10-08 12:53:39 -0400

    modify !stats

--- Commands/stats.js
+++ Commands/stats.js
@@ -4,24 +4,39 @@ module.exports = async function (message, user) {
     let ts = message.createdTimestamp;
     let words = message.content.trim().split(/\s+/)
     let stats = functions.calcEnchants(user);
-    let text = ""
-    for (var enchant in stats) {
-        text += "**" + enchant + "**: " + stats[enchant] + "\n"
-    }
-    //text+="```"
-    functions.sendMessage(message.channel, {
-        "embed": {
-            "title": user.username + "'s Stats",
-            //"description": "Welcome to the blacksmith\nUse `!purchase [ID_of_Item]` to select the item you want to buy!\n",
-            "color": 13498074,
 
-            "fields": [
-                {
-                    "name": "Battle Stats",
-                    "value": text
-                    //"inline": true
-                }
-            ]
+    //text+="```"
+    let categories = [
+        ['attack', 'defense', 'buff', 'dbuff', 'lucky', 'haste'],
+        ['critRate', 'critDamage', 'block', 'pierce', 'lifeSteal', 'sacrifice', 'rage'],
+        ['tempo', 'antitempo', 'combo', 'revenge', 'burn', 'regen', 'evade', 'spikes', 'maxhp'],
+        ['bleed', 'silence', 'vulnerable', 'weakness', 'stun', 'petrify'],
+        ['defmult', 'reflect', 'guard', 'poison', 'survive', 'attackvariance']
+        ['resistance', 'critResist', 'pierceResist', 'silenceResist', 'stunResist', 'petrifyResist']
+    ]
+    let categorynames = ["Main Stats", "1st Gen Enchants", "2nd Gen Enchants", "3rd Gen Enchants", "4th Gen Enchants", "Resistances"]
+    let pages = []
+    for (let i = 0; i < categories.length; i++) {
+        let text = ""
+        for (var enchant in categories[i]) {
+            text += "**" + enchant + "**: " + stats[enchant] + "\n"
         }
-    })
+        let page = {
+            "embed": {
+                "title": user.username + "'s Stats",
+                //"description": "Welcome to the blacksmith\nUse `!purchase [ID_of_Item]` to select the item you want to buy!\n",
+                "color": 13498074,
+
+                "fields": [
+                    {
+                        "name": categorynames[i],
+                        "value": text
+                        //"inline": true
+                    }
+                ]
+            }
+        }
+        pages.push(page)
+    }
+    new functions.Paginator(message.channel, message.author, pages)
 }
