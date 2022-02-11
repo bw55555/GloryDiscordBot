e72d1f35e36c42fddae7515472749f3a6e66f134
bw55555 <brianwang2013@gmail.com>
2022-02-11 17:22:51 -0500

    hotfix

--- Commands/invite.js
+++ Commands/invite.js
@@ -5,7 +5,7 @@ module.exports = async function (message, user) {
   let words = message.content.trim().split(/\s+/)
   functions.sendMessage(message.channel, {
     "embed": {
-      "description": "Invite the bot to your server by clicking [here!](https://discordapp.com/api/oauth2/authorize?client_id=536622022709608468&permissions=8&scope=bot) ",
+          "description": "Invite the bot to your server by clicking [here!](https://discord.com/api/oauth2/authorize?client_id=542757501179789322&permissions=277025392704&scope=bot) ",
       "color": 5251510,
       "timestamp": "2019-01-23T21:42:00.210Z",
       "footer": {
@@ -17,7 +17,7 @@ module.exports = async function (message, user) {
       },
       "author": {
         "name": "Glory - An Discord RPG Bot",
-        "url": "https://discordbots.org/bot/536622022709608468",
+          "url": "https://top.gg/bot/542757501179789322",
         "icon_url": "https://imgur.com/afYUwxM.jpg"
       }
     }
