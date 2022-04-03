cae995102ac6e4edbe40ea45dfef46e79d17e6d1
bw55555 <brianwang2013@gmail.com>
2022-04-03 13:16:07 -0400

    add restore

--- Commands/backup.js
+++ Commands/backup.js
@@ -5,15 +5,13 @@ module.exports = async function (message, user) {
     let words = message.content.trim().split(/\s+/)
     if (admins.indexOf(id) == -1) { return }
     var FS = require('fs');
-    functions.findObjects('userData', {}).then(ret => FS.writeFile('backup/' + ts + '/userData.json', JSON.stringify(ret), (err) => console.log(err)));
-    functions.findObjects('itemData', {}).then(ret => FS.writeFile('backup/' + ts + '/itemData.json', JSON.stringify(ret), (err) => console.log(err)));
-    functions.findObjects('guildData', {}).then(ret => FS.writeFile('backup/' + ts + '/guildData.json', JSON.stringify(ret), (err) => console.log(err)));
-    functions.findObjects('mobData', {}).then(ret => FS.writeFile('backup/' + ts + '/mobData.json', JSON.stringify(ret), (err) => console.log(err)));
-    functions.findObjects('devData', {}).then(ret => FS.writeFile('backup/' + ts + '/devData.json', JSON.stringify(ret), (err) => console.log(err)));
-    functions.findObjects('serverData', {}).then(ret => FS.writeFile('backup/' + ts + '/serverData.json', JSON.stringify(ret), (err) => console.log(err)));
-    functions.findObjects('auctionData', {}).then(ret => FS.writeFile('backup/' + ts + '/auctionData.json', JSON.stringify(ret), (err) => console.log(err)));
-    functions.findObjects('dungeonData', {}).then(ret => FS.writeFile('backup/' + ts + '/dungeonData.json', JSON.stringify(ret), (err) => console.log(err)));
-    functions.findObjects('familiarData', {}).then(ret => FS.writeFile('backup/' + ts + '/familiarData.json', JSON.stringify(ret), (err) => console.log(err)));
+    var foldername = len(words) == 1 ? ts : words[1]
+    collections = ['userData', 'itemData', 'guildData', 'mobData', 'devData', 'serverData', 'auctionData', 'dungeonData', 'familiarData']
+    for (let coll of collections) {
+        functions.findObjects(coll, {}).then(ret => FS.writeFile('backup/' + foldername + '/' + coll + '.json', JSON.stringify(ret), (err) => {
+            if (err) { console.log(err) } else { console.log("Backed up " + coll);}
+        }));
+    }
     functions.replyMessage(message, 'Backup created!')
     functions.logCommand(message)
 }
---
+++ Commands/restore.js
@@ -0,0 +1,37 @@
+
+module.exports = async function (message, user) {
+    let id = message.author.id;
+    let ts = message.createdTimestamp;
+    let words = message.content.trim().split(/\s+/)
+    if (devs.indexOf(id) == -1) { return }
+    var FS = require('fs');
+    if (len(words) == 1) {return functions.replyMessage(message, "Please specify a folder name!")}
+    folder = words[1];
+    collections = ['userData', 'itemData', 'guildData', 'mobData', 'devData', 'serverData', 'auctionData', 'dungeonData', 'familiarData']
+    for (let coll of collections) {
+        let data = FS.readFileSync('backup/' + folder + '/' + coll + '.json', 'utf8')
+        if (data) {
+            let parsed = JSON.parse(data)
+            
+            functions.deleteObjects(coll, {}).then(res => {
+                if (res) {
+                    let tasks = []
+                    for (let backupobj of parsed) {
+                        tasks.push({
+                            insertOne: {
+                                document: backupobj
+                            }
+                        })
+                    }
+                    console.log("Updating " + coll);
+                    functions.bulkWrite(coll, tasks);
+                }
+            })
+            
+        } else {
+            return functions.replyMessage(message, "Missing collection: "+coll)
+        }
+    }
+    functions.replyMessage(message, 'Backup created!')
+    functions.logCommand(message)
+}
